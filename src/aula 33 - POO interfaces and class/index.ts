ea47e4faf9193f2df98b0fa70cc7b97fe068f8ff
Matheus <matheusbastosandrade@gmail.com>
2023-09-27 17:55:23 -0300

    adicionando arquivos da aula 33

---
+++ src/aula 33 - POO interfaces and class/index.ts
@@ -0,0 +1,62 @@
+type VideoPlayerElements = {
+  buttonStop: HTMLButtonElement;
+  buttonPlay: HTMLButtonElement;
+  videoPlayer: HTMLVideoElement;
+};
+
+type VideoPlayerProtocol = {
+  play(): void;
+  pause(): void;
+  startEvents(): void;
+};
+
+// classe video deve ter os métodos play, pause e startevents, por que implementa o type VideoPlayerProtocol. Além de só ser istanciada com um objeto que tenha os atributos do type VideoPlayerElements.
+
+class Video implements VideoPlayerProtocol {
+  private videoPlayer: HTMLVideoElement;
+  private buttonStop: HTMLButtonElement;
+  private buttonPlay: HTMLButtonElement;
+
+  constructor(HTMLElements: VideoPlayerElements) {
+    this.buttonPlay = HTMLElements.buttonPlay;
+    this.buttonStop = HTMLElements.buttonStop;
+    this.videoPlayer = HTMLElements.videoPlayer;
+  }
+
+  play(): void {
+    this.videoPlayer.play();
+  }
+
+  pause(): void {
+    this.videoPlayer.pause();
+  }
+
+  startEvents(): void {
+    this.buttonPlay.addEventListener("click", () => {
+      if (this.videoPlayer.paused) {
+        this.play();
+        this.buttonPlay.textContent = "Pause";
+        return;
+      }
+
+      this.pause();
+      this.buttonPlay.textContent = "Play";
+    });
+
+    this.buttonStop.addEventListener("click", () => {
+      this.pause();
+      this.videoPlayer.currentTime = 0;
+      this.buttonPlay.textContent = "Play";
+    });
+  }
+}
+
+const protocols = {
+  buttonPlay: document.querySelector(".play") as HTMLButtonElement,
+  buttonStop: document.querySelector(".stop") as HTMLButtonElement,
+  videoPlayer: document.querySelector("video") as HTMLVideoElement,
+};
+
+const video = new Video(protocols);
+
+video.startEvents();
